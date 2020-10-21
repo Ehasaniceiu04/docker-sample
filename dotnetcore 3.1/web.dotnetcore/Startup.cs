da685444fc198c8e77bfea1d1e2d950f2d94af81
ehasaniceiu04 <ehasan@outlook.com>
2020-10-21 14:14:46 -0400

    dotnet core 3.1 docker file added

--- dotnetcore 3.1/web.dotnetcore/Startup.cs
+++ dotnetcore 3.1/web.dotnetcore/Startup.cs
@@ -36,11 +36,11 @@ namespace web.dotnetcore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
